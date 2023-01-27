dd9d0dc56a660c019de99e12dda0ff23996a853f
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-01-26 19:04:30 -0600

    feat: 🎸 Adding response interface

--- api/models/index.ts
+++ api/models/index.ts
@@ -6,3 +6,10 @@ export { Post } from './posts.model'
 export { Role } from './roles.model'
 export { Setting } from './settings.model'
 export { User } from './users.model'
+
+export interface response {
+  total?: number
+  limit?: number
+  skip?: number
+  data?: Array<any | object>
+}
